511ab76792dc195894a9d1e8820446d11bcbd7ff
LKriszgruber <l.kriszgruber@gmail.com>
2024-06-24 14:01:24 +0200

    Scrapper + refactoring

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -1,27 +1,36 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
-
 class Scrapper:
 
-    def get_url_sale(self):
-        pass
+    def __init__(self) -> None:
+        """
+        Function that set up the base of the scrapping
+        """
+        self.start_url = "https://www.immoweb.be/en/search/"
 
-    def set_up(self):
-        self.start_url = "https://www.immoweb.be/en"
-        self.buy = "#homepage-tab-buy"
+        # Work around for the restriction where immoweb don't allow request that aren't coming from a browser
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Referer": self.start_url,
+        }
 
-        self.driver = webdriver.Chrome()
-        self.driver.get(self.start_url)
-
-        self.cookie_button = self.driver.find_element(
-            By.XPATH, " //button[@data-testid='uc-accept-all-button']"
+    def get_url_sale(self, type):
+        self.type = type
+        self.to_buy = "/for-sale"
+        self.url = (
+            "https://www.immoweb.be/en/classified/house/for-sale/nassogne/6950/11485698"
         )
-        self.cookie_button.click()
+        # r = requests.get(f"{self.start_url}{self.type}{self.buy}")
+        self.r = requests.get(f"{self.url}", headers=self.headers)
+        self.r.raise_for_status()
+        self.soup = BeautifulSoup(self.r.content, "html.parser").get_text()
+        self.soup = re.sub("\n\n", "", self.soup)
+        print(self.soup)
 
-        # self.search_button = self.driver.find_element(
-        #     By.XPATH, "//button[@id='searchBoxSubmitButton']"
-        # )
-        # self.url = self.driver.current_url
-        # self.driver.close()
+    def save(self):
+        with open("test.json", "w") as file:
+            file = json.dump(self.soup, file)
+
+
+if __name__ == "__main__":
+    s = Scrapper()
+    s.set_up()
+    s.save()
