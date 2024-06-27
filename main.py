50afe93d61820e2fb684f13a7560ce4b4e9d2770
LKriszgruber <l.kriszgruber@gmail.com>
2024-06-27 14:15:32 +0200

    scrapper done

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import os
-import requests
-from bs4 import BeautifulSoup
-import time
 import pandas as pd
-import json
-import re
-from bs4 import BeautifulSoup
-import requests
 
 from utils.scrapper import Scrapper
 
@@ -14,7 +7,7 @@ from utils.scrapper import Scrapper
 def main() :
     os.system("cls")
     s = Scrapper()
-    s.get_url_sale()
+    s.start_scrapping()
 
 if __name__ == '__main__':
     main()
